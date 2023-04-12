4debc61b9c01d75426c590852fa0a24af44f93f8
Artyom <mr.ab.ab.1910@mail.ru>
2023-04-13 00:55:10 +1000

    Initial commit

---
+++ src/Controller/FileCount.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class FileCount extends AbstractController
+{
+    public function countFiles(): Response
+    {
+        $path = 'C:\ДЗ\5\TEST';
+        $total = 0;
+        $iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path));
+        
+        foreach ($iterator as $file) {
+            if ($file->isFile() && $file->getBasename() === 'count') {
+                $openfile = fopen($file, 'r');
+                while (($line = fgets($openfile)) !== false) {
+                    $number = intval($line);
+                    if ($number) $total += $number;
+                }
+                fclose($openfile);
+            }
+        }
+        
+        return new Response('Total: '.$total);
+    }
+
+
+}
